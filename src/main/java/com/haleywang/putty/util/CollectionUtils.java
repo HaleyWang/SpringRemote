30785959d532f62ef2b11da79497058b729fd079
Haley Wang <0400150222@163.com>
2019-05-12 10:54:15 +0800

    Action Dialog

--- src/main/java/com/haleywang/putty/util/CollectionUtils.java
+++ src/main/java/com/haleywang/putty/util/CollectionUtils.java
@@ -1,6 +1,5 @@
 package com.haleywang.putty.util;
 
-import com.sun.istack.internal.Nullable;
 
 import java.util.List;
 
@@ -8,7 +7,6 @@ public class CollectionUtils {
 
     private CollectionUtils(){}
 
-    @Nullable
     public static  <T> T getItem(List<T> list, int index) {
         if(list == null || index < 0 || index >= list.size()) {
             return null;
